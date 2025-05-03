1fba9f953a042c8308dbfd67d279785813fa1a0a
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-03 17:25:04 +0800

    mynext init 3

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,19 @@ export default function Navbar() {
     <div>
       <nav className="max-w-3xl mx-auto py-4 flex gap-x-4">
         <Link href="/">Home</Link>
-        <Link href="/about">about</Link>
+        <Link href="/sub1">Sub1</Link>
+        <Link href="/sub2">Sub2</Link>
+        <Link href="/sub3">Sub3</Link>
+        <Link href="/sub4">Sub4</Link>
+        <Link href="/sub5">Sub5</Link>
+        <Link href="/sub6">Sub6</Link>
+        <Link href="/sub7">Sub7</Link>
+        <Link href="/sub8">Sub8</Link>
+        <Link href="/sub9">Sub9</Link>
+        <Link href="/sub10">Sub10</Link>
+        <Link href="/sub11">Sub11</Link>
+        <Link href="/sub12">Sub12</Link>
+        <Link href="/about">About</Link>
       </nav>
     </div>
   );
---
+++ app/sub12/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function sub12() {
+  return (
+    <div className="max-w-3xl mx-auto py-4 flex gap-x-4">
+      sub12
+    </div>
+  )
+}
+
