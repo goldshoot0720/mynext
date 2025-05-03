913f86375aafac314cb8f3e06f606bb2870528b3
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-03 17:17:43 +0800

    mynext init 2

---
+++ mynext git.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "mynext init 2"
+git remote add origin https://github.com/goldshoot0720/mynext.git
+git checkout -b master
+git push -u origin master --force
+pause
