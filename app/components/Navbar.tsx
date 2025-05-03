1ff7f329aacc5ba53a05d4e0829ffe15499dd798
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-03 20:10:56 +0800

    mynext init 7

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 export default function Navbar() {
   return (
       <nav className="max-w-3xl mx-auto py-4 flex gap-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/sub1">Sub1</Link>
-        <Link href="/sub2">Sub2</Link>
-        <Link href="/sub3">Sub3</Link>
-        <Link href="/sub4">Sub4</Link>
-        <Link href="/sub5">Sub5</Link>
-        <Link href="/sub6">Sub6</Link>
-        <Link href="/sub7">Sub7</Link>
-        <Link href="/sub8">Sub8</Link>
-        <Link href="/sub9">Sub9</Link>
-        <Link href="/sub10">Sub10</Link>
-        <Link href="/sub11">Sub11</Link>
-        <Link href="/sub12">Sub12</Link>
-        <Link href="/about">About</Link>
+        <Link className="hover:text-blue-500" href="/">Home</Link>
+        <Link className="hover:text-blue-500" href="/sub1">Sub1</Link>
+        <Link className="hover:text-blue-500" href="/sub2">銀行</Link>
+        <Link className="hover:text-blue-500" href="/sub3">Sub3</Link>
+        <Link className="hover:text-blue-500" href="/sub4">Sub4</Link>
+        <Link className="hover:text-blue-500" href="/sub5">Sub5</Link>
+        <Link className="hover:text-blue-500" href="/sub6">Sub6</Link>
+        <Link className="hover:text-blue-500" href="/sub7">Sub7</Link>
+        <Link className="hover:text-blue-500" href="/sub8">Sub8</Link>
+        <Link className="hover:text-blue-500" href="/sub9">Sub9</Link>
+        <Link className="hover:text-blue-500" href="/sub10">Sub10</Link>
+        <Link className="hover:text-blue-500" href="/sub11">Sub11</Link>
+        <Link className="hover:text-blue-500" href="/sub12">Sub12</Link>
+        <Link className="hover:text-blue-500" href="/about">About</Link>
       </nav>
   );
 }
